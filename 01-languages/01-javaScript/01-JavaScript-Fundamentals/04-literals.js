36b2343d35bfe98472068dc3b3d546ce2a48318c
Dalaktronixs <hakeemabdullah87@gmail.com>
2025-06-01 18:38:15 +0100

    FEAT: add the 04-literals.js file

---
+++ 01-languages/01-javaScript/01-JavaScript-Fundamentals/04-literals.js
@@ -0,0 +1,34 @@
+
+
+//Syntax for creating Literals
+
+let msg = `Hello World`  //Regular Text with Literal Templates
+
+const age = 34;
+const name = "Abdulhakeem";
+const occupation = "Software Engineer"
+
+//Injecting Variables inside a string using Literal Templates
+
+let message = `My name is ${name}, I am ${age} years old. I am a ${occupation}`;
+console.log(message);
+
+
+//For multiline message or text
+let textmsg = `I am a software engineer in the making,
+I have always dream of becoming this since 2016 but always fail.
+I think Insha Allah I must succeed year 2025`
+
+console.log(textmsg);
+
+//Formating Number with intl.Number in Template Literals
+
+const amount = 370000;
+//TRICK 1
+console.log(new Intl.NumberFormat().format(amount)); //Does the grouping for me in commas
+
+//TRICK 2
+console.log(new Intl.NumberFormat("en-NG", {style: "currency", currency: "NGN"}).format(amount));
+
+
+
--- 01-languages/01-javaScript/01-JavaScript-Fundamentals/challenge.js
+++ 01-languages/01-javaScript/01-JavaScript-Fundamentals/challenge.js
@@ -18,4 +18,19 @@ const person2 = new Person("John", 92, 1.95);
 const BMIMarks = person1.computeBMI();
 console.log("Marks BMI: ", BMIMarks);
 const BMIJohn = person2.computeBMI();
-console.log("John BMI: ", BMIJohn);
+console.log("John BMI: ", BMIJohn); 
+
+//Using the Decision Marking Statement
+// if (BMIMarks > BMIJohn){
+//     console.log("Mark's BMI is greater the John's")
+// } else{
+//     console.log("John's BMI is greater the Mark's")
+// }
+//Using Template String
+
+if (BMIMarks > BMIJohn){
+    console.log(`Mark's BMI (${BMIMarks}) is higher than John's BMI ${BMIJohn}`);
+} else{
+    console.log(`John's BMI (${BMIJohn}) is higher than Marks's BMI (${BMIMarks})`);
+}
+
